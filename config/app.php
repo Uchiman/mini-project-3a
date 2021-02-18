a7538229ff7b7e793ac5c667d4e4d66a9e232755
Uchiman <utsman.khabillah@gmail.com>
2021-02-18 21:05:29 +0700

    bismillah

--- config/app.php
+++ config/app.php
@@ -167,8 +167,6 @@ return [
          */
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
-
         
         /*
          * Application Service Providers...
@@ -232,7 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
 
     ],
 
